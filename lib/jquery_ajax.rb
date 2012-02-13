8ae40646a8797dfab686b3bd8f7238f88db47cc0
solaiman shaikh <solaiman@abdulmotaleb.(none)>
2012-02-13 06:57:50 +0530

    added some new ajax method.

--- lib/jquery_ajax.rb
+++ lib/jquery_ajax.rb
@@ -13,6 +13,8 @@ module JqueryAjax
       html << "#{hide(opts[:target])}"
     when "toggle"
       html << "#{toggle(opts[:target])}"
+    when "ajax"
+      html << "#{ajax(opts[:id], opts[:target])}"
     end
     
     html << " return false;});"
@@ -22,6 +24,25 @@ module JqueryAjax
 
 
   def onchange_ajax(args={})
+    html = []
+    html << "#{link_to opts[:name],'#',:id => opts[:id]}"
+    html << "<script>"
+    html << "$('##{opts[:id]}').change(function(){"
+
+    case opts[:target][:event]
+    when "show"
+      html << "#{show(opts[:target])}"
+    when "hide"
+      html << "#{hide(opts[:target])}"
+    when "toggle"
+      html << "#{toggle(opts[:target])}"
+    when "ajax"
+      html << "#{ajax(opts[:id], opts[:target])}"
+    end
+
+    html << " return false;});"
+    html << "</script>"
+    html
     
   end
 
@@ -48,6 +69,21 @@ module JqueryAjax
   def toggle(args={})
     html = []
     html = "$('##{args[:id]}').toggle();"
+    html
+  end
+
+  def ajax(id=nil, args={})
+    html = []
+    html << "$.ajax({"
+    html << "url: '#{args[:url]}',"
+    html << "dataType: 'html',"
+    html << "beforeSend: function( xhr ) {"
+    html << "xhr.overrideMimeType( 'text/plain; charset=x-user-defined' );"
+    html << " },"
+    html << "success: function( data ) {"
+    html << "$('##{args[:id]}').html('data');"
+    html << " }"
+    html << " });"
     html
   end
 end
